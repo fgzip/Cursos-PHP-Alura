09b7efc7a118bd02216720e606c2161163f43c4d
fgzip <64863132+fgzip@users.noreply.github.com>
2020-05-05 15:22:59 -0300

    Add files via upload
    Arquivos do primeiro curso de PHP da Alura

---
+++ 07_tabuada.php
@@ -0,0 +1,11 @@
+<?php
+
+$multiplicador = 3;
+echo "\n";
+
+for($i = 1; $i <= 10; $i++) {
+    $resultado = $multiplicador * $i;
+    echo "$multiplicador x $i = $resultado" . PHP_EOL;
+}
+
+echo "\n";
